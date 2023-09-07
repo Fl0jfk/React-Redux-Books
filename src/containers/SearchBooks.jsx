54f3827f736809895bef23134c3c4b9cb7a5f62a
Fl0jfk <florian.hacqueville@hotmail.fr>
2023-09-07 11:37:08 +0200

    add toastify anim n google API

--- src/containers/SearchBooks.jsx
+++ src/containers/SearchBooks.jsx
@@ -1,12 +1,88 @@
-import { useState } from "react"
+import { useState } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchBooks } from '../redux/actions/actionFetchBooks';
+import { addBook } from '../redux/actions/actionAddBooks';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 function SearchBooks () {
-    /* https://www.googleapis.com/books/v1/volumes?q=${title}&maxResults=20 */
     const [title, setTitle] = useState('');
+    const state = useSelector(state => state.search);
+    const dispatch = useDispatch();
+    const notify = () => toast("Livre enregistré");
 
     const handleSubmit = e => {
         e.preventDefault();
+        dispatch(fetchBooks(title))
     }
+
+    const handleSave = (title, author) => {
+        const bookToSave ={
+            title: title,
+            author: author
+        }
+        dispatch(addBook(bookToSave))
+        notify()
+    }
+
+    const displayFetchedBooks = state.isLoading ? (
+        <div className="d-flex justify-content-center">
+            <div className="spinner-border text-info" role="status">
+                <span className="sr-only"></span>
+            </div>
+        </div>
+    )
+    : 
+    state.error !== ''? (
+        <p>{state.error}</p>
+    )
+    : (
+        state.fetchedBooks.map(data => {
+            return (
+                <div className="accordion-item" key={data.id}>
+                    <h2 className="accordion-header" id={`heading${data.id}`}>
+                        <button 
+                            className="accordion-button" 
+                            type="button"
+                            data-bs-toggle="collapse" 
+                            data-bs-target={`#collapse${data.id}`}
+                            aria-expanded="true"
+                            aria-controls={`collapse${data.id}`}
+                        >
+                        {data.volumeInfo.title}
+                        </button>
+                    </h2>
+                    <div id={`collapse${data.id}`} className="accordion-collapse collapse show" aria-labelledby={`heading${data.id}`} data-bs-parent="#accordion">
+                        <div className="accordion-body">
+                            {
+                                data.volumeInfo.hasOwnProperty('imageLinks') &&
+                                <img src={data.volumeInfo.imageLinks.thumbnail} alt={`Couverture du livre : ${data.volumeInfo.title}`}/>
+                            }
+                            <br />
+                            <h4 className="card-title mt-3">Titre :  {data.volumeInfo.title}</h4>
+                            <h5 className="card-title mt-3">Auteurs : {data.volumeInfo.authors}</h5>
+                            <p className="card-text mt-3">Description : {data.volumeInfo.description}</p>
+                            <a 
+                                className="btn btn-outline-secondary" 
+                                target="blank" rel="noopener noreferrer" 
+                                href={data.volumeInfo.previewLink}
+                            >
+                                Plus d'infos
+                            </a>
+                            <button 
+                                className="btn btn-outline-secondary ms-3"
+                                onClick={()=> handleSave(data.volumeInfo.title, data.volumeInfo.authors)}
+                            >
+                                Enregistrer
+                            </button>
+                            <ToastContainer />
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+       
+    ) 
 
     return (
         <main role="main">
@@ -32,25 +108,8 @@ function SearchBooks () {
                 </div>
             </div>
             <div className="container mt-5" style={{minHeight: '200px'}}>
-                <div className="accordion">
-                    <div className="card mb-2">
-                            <div className="card-header">
-             
-                            </div>
-                            <div className="collapse" data-parent="accordion">
-                                <div className="card-body">
-                                    {
-                                        /*
-                                        -img
-                                        -title
-                                        
-                                        author
-                                        des
-                                        */
-                                    }
-                                </div>
-                            </div>
-                    </div>
+                <div id="accordion" className="accordion">
+                   {displayFetchedBooks}
                 </div>
             </div>
         </main>
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,11 +1,13 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import reducerAddBooks from './reducers/reducerAddBooks';
-
+import thunk from 'redux-thunk';
+import reducerFetchedBooks from './reducers/reducerFetchBooks';
 
 const rootReducer = combineReducers({
-    library: reducerAddBooks
+    library: reducerAddBooks,
+    search : reducerFetchedBooks
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
